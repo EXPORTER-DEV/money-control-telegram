7b31cb43d8ec419d74677a6c937adfd58f9d0b00
Vadim <mrdrakan@Mail.ru>
2022-06-27 21:54:10 +0300

    feat(user): added middleware for saving user in database

---
+++ src/middlewares/user/user.ts
@@ -0,0 +1,29 @@
+import { plainToInstance } from "class-transformer";
+import { UserModel } from "../../database/models/user.model";
+import { ILogger } from "../../lib/logger/logger";
+import { IDatabaseContext } from "../database/database.interface";
+
+export class User {
+    logger: ILogger;
+    constructor(logger: ILogger){
+        this.logger = logger.child({module: 'User'});
+    }
+    init(){
+        return async (ctx: IDatabaseContext, next: Function) => {
+            if(ctx.from){
+                const userModel = ctx.database!.inject<UserModel>(UserModel);
+                try {
+                    const find = await userModel.findById(ctx.from.id);
+                    if(find !== undefined){
+                        const user = plainToInstance(userModel.model, {id: ctx.from.id});
+                        await user.save();
+                        this.logger.info(`Successfully saved ${user.id} user.`);
+                    }
+                } catch (e: any) {
+                    this.logger.error(`Failed init middleware: ${e.stack}`);
+                }
+            }
+            await next();
+        }
+    }
+}
