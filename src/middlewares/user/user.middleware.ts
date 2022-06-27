17342a1b41f722618a3fafa343c3b70f801addbe
Vadim <mrdrakan@Mail.ru>
2022-06-27 21:54:22 +0300

    fix(user middleware)

--- src/middlewares/user/user.middleware.ts
+++ src/middlewares/user/user.middleware.ts
@@ -1,6 +1,7 @@
 import { UserModel } from "../../database/models/user.model";
 import { ILogger } from "../../lib/logger/logger";
 import { IDatabaseContext } from "../database/database.interface";
+import { IUserContext } from "./user.interface";
 
 export class UserMiddleware {
     logger: ILogger;
@@ -8,16 +9,19 @@ export class UserMiddleware {
         this.logger = logger.child({module: 'User'});
     }
     init(){
-        return async (ctx: IDatabaseContext, next: () => void) => {
+        return async (ctx: IUserContext, next: () => void) => {
             if(ctx.from){
                 const userModel = ctx.database!.inject<UserModel>(UserModel);
                 try {
                     const find = await userModel.findById(ctx.from.id);
-                    if(find !== undefined){
+                    if(find === undefined){
                         const user = new userModel.model();
                         user.id = ctx.from.id;
                         await user.save();
                         this.logger.info(`Successfully saved ${user.id} user.`);
+                        ctx.user = user;
+                    } else {
+                        ctx.user = find;
                     }
                 } catch (e: any) {
                     this.logger.error(`Failed init middleware: ${e.stack}`);
