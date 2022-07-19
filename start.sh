8ae4dcb25bea78f0db7570723b0c46e9f5fac4d0
Vadim <mrdrakan@Mail.ru>
2022-07-20 00:52:36 +0300

    build

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Docker up
+npm ci --only=production --ignore-scripts
 npm run build
 docker-compose -f docker-compose.yml build --no-cache
 docker-compose -f docker-compose.yml up -d --force-recreate
