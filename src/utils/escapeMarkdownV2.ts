be0e338931214d6372d0ee8e36b7abcae1061e75
Vadim <mrdrakan@Mail.ru>
2023-01-27 19:11:24 +0300

    fix: improve escapeMarkdownV2

--- src/utils/escapeMarkdownV2.ts
+++ src/utils/escapeMarkdownV2.ts
@@ -1,3 +1,22 @@
+const MARKDOWN_REPLACEMENTS: [
+    RegExp, string
+][] = [
+    [
+        /\./gi,  '\\.',
+    ],
+    [
+        /-/gi, '\\-'
+    ],
+    [
+        /\(/gi, '\\(',
+    ],
+    [
+        /\)/gi, '\\)',
+    ],
+];
+
 export const escapeMarkdownV2 = (message: string): string => {
-    return message.replace(/\./gi, '\\.').replace(/-/gi, '\\-');
+    return MARKDOWN_REPLACEMENTS.reduce<string>((carry, [search, replace]) => {
+        return carry.replace(search, replace);
+    }, message);
 };
